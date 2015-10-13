cfe4cd5627d12f063cdf85454a342944aa2b5052
mingl0280 <PMX>
2015-10-12 20:00:13 -0600

    init submit
    Signed-off-by: mingl0280 <PMX>

---
+++ LDAP_SET_ATTRIBUTE/Form1.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意:  以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.TextBox1.Location = New System.Drawing.Point(0, 29)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox1.Size = New System.Drawing.Size(407, 232)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(0, 2)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(310, 21)
+        Me.TextBox2.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(320, 0)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "GOSearch"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(407, 261)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+End Class
---
+++ LDAP_SET_ATTRIBUTE/Form1.vb
@@ -0,0 +1,83 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Text
+Imports System.DirectoryServices
+Imports System.DirectoryServices.ActiveDirectory
+
+
+Public Class Form1
+
+    Private DEntry As DirectoryEntry = New DirectoryEntry("LDAP://LocalHost", "Administrator", "asdf4321!!", AuthenticationTypes.Secure)
+    Private TimeCode_Staff As String = "0 0 0 0 192 255 0 192 255 0 192 255 0 192 255 0 192 255 0 0 0"
+    Private TimeCode_Executive As String = "255 31 248 255 255 255 255 255 255 255 255 255 255 255 255 255 255 255 255 255 255"
+    Private TimeCode_Manager As String = "3 0 0 0 192 255 3 192 255 3 192 255 3 192 255 3 192 255 3 192 255"
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox1.Text += "Loading..."
+        Dim testdel = getDirectoryEntry("(&(objectClass=user))")
+
+        For Each elem In testdel
+            Dim suc = SetUserDtAttribute(elem)
+            If suc Then TextBox1.Text += "--->Successful" Else TextBox1.Text += "--->Failed."
+            TextBox1.Text += vbCrLf
+        Next
+
+    End Sub
+
+    Private Function SetUserDtAttribute(ByRef lin As DirectoryEntry) As Boolean
+        Try
+            Dim MoF As String = ""
+            MoF = lin.Properties("memberOf").Value
+            If MoF.Contains("StaffMembers") Then
+                TextBox1.Text += lin.Name + " is a STAFF MEMBER, SETTING LOGIN TIME: 7AM – 5PM on Monday – Friday."
+                SetLogonHours(lin, TimeCode_Staff)
+                lin.InvokeSet("logonHours", DirectCast(TimeCode_Staff, Object))
+            ElseIf MoF.Contains("Manager") Then
+                TextBox1.Text += lin.Name + " is a MANAGER, SETTING LOGIN TIME: Monday – Saturday from 7AM – 7PM."
+                SetLogonHours(lin, TimeCode_Manager)
+            ElseIf MoF.Contains("Executive") Then
+                TextBox1.Text += lin.Name + " is an EXECUTIVE MEMBER, SETTING LOGIN TIME: 24/7, but are not allowed to on Sunday from 6AM – 12PM."
+                SetLogonHours(lin, TimeCode_Executive)
+            End If
+            lin.CommitChanges()
+            Return True
+        Catch ex As Exception
+            TextBox1.Text += ex.Message + ex.Source + ex.StackTrace
+            Return False
+        End Try
+    End Function
+
+    Private Sub SetLogonHours(ByRef ent As DirectoryEntry, ByVal TimeValue As String)
+        If ent.Properties.Contains("logonHours") Then
+            ent.Properties("logonHours").Value = TimeValue
+        Else
+            ent.Properties("logonHours").Add(TimeValue)
+        End If
+    End Sub
+
+    Private Function getDirectoryEntry(ByVal FilterString As String) As List(Of DirectoryEntry)
+        Dim DSearcher As DirectorySearcher = New DirectorySearcher(DEntry)
+        TextBox1.Text += "ADS Load" + vbCrLf
+        DSearcher.Filter = FilterString
+        TextBox1.Text += "Filter Filted" + vbCrLf
+        DSearcher.SearchScope = SearchScope.Subtree
+        Dim Sresult() As SearchResult
+        Dim SResultCollection As SearchResultCollection = DSearcher.FindAll()
+        ReDim Sresult(SResultCollection.Count - 1)
+        TextBox1.Text += "search over" + vbCrLf
+        SResultCollection.CopyTo(Sresult, 0)
+        Dim RetDEList As New List(Of DirectoryEntry)
+        TextBox1.Text += SResultCollection.Count.ToString + vbCrLf
+        For i As Integer = 0 To UBound(Sresult)
+            RetDEList.Add(New DirectoryEntry(Sresult(i).Path))
+            'TextBox1.Text += Sresult(i).Path + vbCrLf
+        Next
+        Return RetDEList
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        getDirectoryEntry(TextBox2.Text)
+    End Sub
+End Class
