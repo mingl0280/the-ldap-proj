af53cd635c28cdd40017d6f9681b4072b0d77e87
mingl0280 <PMX>
2015-10-15 23:50:00 -0600

    Added function to add homedrive and homedirectory
    Signed-off-by: mingl0280 <PMX>

--- LDAP_SET_ATTRIBUTE/Form1.Designer.vb
+++ LDAP_SET_ATTRIBUTE/Form1.Designer.vb
@@ -25,39 +25,54 @@ Partial Class Form1
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.TextBox2 = New System.Windows.Forms.TextBox()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'TextBox1
         '
-        Me.TextBox1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.TextBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.TextBox1.Location = New System.Drawing.Point(0, 29)
         Me.TextBox1.Multiline = True
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
-        Me.TextBox1.Size = New System.Drawing.Size(407, 232)
+        Me.TextBox1.Size = New System.Drawing.Size(751, 232)
         Me.TextBox1.TabIndex = 0
         '
         'TextBox2
         '
+        Me.TextBox2.Enabled = False
         Me.TextBox2.Location = New System.Drawing.Point(0, 2)
         Me.TextBox2.Name = "TextBox2"
         Me.TextBox2.Size = New System.Drawing.Size(310, 21)
         Me.TextBox2.TabIndex = 1
+        Me.TextBox2.Text = "(&(objectClass=user))"
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(320, 0)
+        Me.Button1.Location = New System.Drawing.Point(328, 2)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.Size = New System.Drawing.Size(151, 23)
         Me.Button1.TabIndex = 2
-        Me.Button1.Text = "GOSearch"
+        Me.Button1.Text = "Set Account Logon Time"
         Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(485, 2)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(205, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Set HomeDrive and HomeDirectory"
+        Me.Button2.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(407, 261)
+        Me.ClientSize = New System.Drawing.Size(752, 261)
+        Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.TextBox1)
@@ -70,5 +85,6 @@ Partial Class Form1
     Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
     Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
     Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
 
 End Class
--- LDAP_SET_ATTRIBUTE/Form1.vb
+++ LDAP_SET_ATTRIBUTE/Form1.vb
@@ -3,6 +3,7 @@ Imports System.Collections.Generic
 Imports System.Text
 Imports System.DirectoryServices
 Imports System.DirectoryServices.ActiveDirectory
+Imports System.Text.Encoding
 
 
 Public Class Form1
@@ -13,35 +14,83 @@ Public Class Form1
     Private TimeCode_Manager As String = "3 0 0 0 192 255 3 192 255 3 192 255 3 192 255 3 192 255 3 192 255"
 
 
-
+    ''' <summary>
+    ''' Nothing important...
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         TextBox1.Text += "Loading..."
-        Dim testdel = getDirectoryEntry("(&(objectClass=user))")
+    End Sub
+
+    ''' <summary>
+    ''' The sub that calls the program to set Logon Time for each user
+    ''' </summary>
+    ''' <param name="input">AD Filter String</param>
+    ''' <remarks></remarks>
+    Sub BeginCall(ByVal input As String)
+        Dim testdel = getDirectoryEntry(input)
 
         For Each elem In testdel
             Dim suc = SetUserDtAttribute(elem)
             If suc Then TextBox1.Text += "--->Successful" Else TextBox1.Text += "--->Failed."
             TextBox1.Text += vbCrLf
         Next
-
     End Sub
 
-    Private Function SetUserDtAttribute(ByRef lin As DirectoryEntry) As Boolean
+    ''' <summary>
+    ''' The sub that calls the program to set Home Drive and Home Directory for each user
+    ''' </summary>
+    ''' <param name="input">AD Filter String</param>
+    ''' <remarks></remarks>
+    Sub CSetHomeDrive(ByVal input As String)
+        Dim testdel = getDirectoryEntry(input)
+
+        For Each elem In testdel
+            Dim suc = SetUserHDrAttribute(elem)
+            If suc Then TextBox1.Text += "--->Successful" Else TextBox1.Text += "--->Failed."
+            TextBox1.Text += vbCrLf
+        Next
+    End Sub
+
+    ''' <summary>
+    ''' Convert a Octect String into byte array
+    ''' </summary>
+    ''' <param name="input"></param>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Private Function OctectStringToBytes(ByVal input As String) As Byte()
+        Dim strarr() As String = input.Split(" ")
+        Dim byteArr() As Byte
+        ReDim byteArr(UBound(strarr))
+        For i = 0 To UBound(strarr)
+            byteArr(i) = CByte(strarr(i))
+        Next
+        Return byteArr
+    End Function
+
+    ''' <summary>
+    ''' Set the LogonTime for specified user
+    ''' </summary>
+    ''' <param name="din">the DirectoryEntry for input, which is a record of the Account</param>
+    ''' <returns>The Operation is successful or not</returns>
+    ''' <remarks></remarks>
+    Private Function SetUserDtAttribute(ByRef din As DirectoryEntry) As Boolean
         Try
             Dim MoF As String = ""
-            MoF = lin.Properties("memberOf").Value
+            MoF = din.Properties("memberOf").Value
             If MoF.Contains("StaffMembers") Then
-                TextBox1.Text += lin.Name + " is a STAFF MEMBER, SETTING LOGIN TIME: 7AM – 5PM on Monday – Friday."
-                SetLogonHours(lin, TimeCode_Staff)
-                lin.InvokeSet("logonHours", DirectCast(TimeCode_Staff, Object))
+                TextBox1.Text += din.Name + " is a STAFF MEMBER, SETTING LOGIN TIME: 7AM – 5PM on Monday – Friday."
+                SetLogonHours(din, TimeCode_Staff)
             ElseIf MoF.Contains("Manager") Then
-                TextBox1.Text += lin.Name + " is a MANAGER, SETTING LOGIN TIME: Monday – Saturday from 7AM – 7PM."
-                SetLogonHours(lin, TimeCode_Manager)
+                TextBox1.Text += din.Name + " is a MANAGER, SETTING LOGIN TIME: Monday – Saturday from 7AM – 7PM."
+                SetLogonHours(din, TimeCode_Manager)
             ElseIf MoF.Contains("Executive") Then
-                TextBox1.Text += lin.Name + " is an EXECUTIVE MEMBER, SETTING LOGIN TIME: 24/7, but are not allowed to on Sunday from 6AM – 12PM."
-                SetLogonHours(lin, TimeCode_Executive)
+                TextBox1.Text += din.Name + " is an EXECUTIVE MEMBER, SETTING LOGIN TIME: 24/7, but are not allowed to on Sunday from 6AM – 12PM."
+                SetLogonHours(din, TimeCode_Executive)
             End If
-            lin.CommitChanges()
+            din.CommitChanges()
             Return True
         Catch ex As Exception
             TextBox1.Text += ex.Message + ex.Source + ex.StackTrace
@@ -49,35 +98,95 @@ Public Class Form1
         End Try
     End Function
 
+    ''' <summary>
+    ''' Set the HomeDrive and HomeDirectory for specified user
+    ''' </summary>
+    ''' <param name="din">the DirectoryEntry for input, which is a record of the Account</param>
+    ''' <returns>The Operation is successful or not</returns>
+    ''' <remarks></remarks>
+    Private Function SetUserHDrAttribute(ByRef din As DirectoryEntry) As Boolean
+        Try
+            Dim MoF As String = ""
+            MoF = din.Properties("memberOf").Value
+            If MoF.Contains("StaffMembers") Or MoF.Contains("Manager") Then
+                TextBox1.Text += din.Name + " is a STAFF MEMBER, SETTING Home Drive S"
+                SetHomeDriveProperty(din, "S")
+            ElseIf MoF.Contains("Executive") Then
+                TextBox1.Text += din.Name + " is an EXECUTIVE MEMBER, SETTING Home Drive O"
+                SetHomeDriveProperty(din, "O")
+            End If
+            din.CommitChanges()
+            Return True
+        Catch ex As Exception
+            TextBox1.Text += ex.Message + ex.Source + ex.StackTrace
+            Return False
+        End Try
+    End Function
+
+    ''' <summary>
+    ''' Change the LogonHour for specified DirectoryEntry
+    ''' </summary>
+    ''' <param name="ent">The Entry that needs to be changed</param>
+    ''' <param name="TimeValue">The Octect String Value</param>
+    ''' <remarks></remarks>
     Private Sub SetLogonHours(ByRef ent As DirectoryEntry, ByVal TimeValue As String)
         If ent.Properties.Contains("logonHours") Then
-            ent.Properties("logonHours").Value = TimeValue
+            ent.Properties("logonHours").Value = OctectStringToBytes(TimeValue)
         Else
-            ent.Properties("logonHours").Add(TimeValue)
+            ent.Properties("logonHours").Add(OctectStringToBytes(TimeValue))
         End If
     End Sub
 
+    ''' <summary>
+    ''' Change the HomeDrive and HomeDirectory value for a DirectoryEntry
+    ''' </summary>
+    ''' <param name="ent">The Entry that needs to be changed</param>
+    ''' <param name="letter">The HomeDrive letter desiered to set for the Entry</param>
+    ''' <remarks></remarks>
+    Private Sub SetHomeDriveProperty(ByRef ent As DirectoryEntry, ByVal letter As String)
+        If ent.Properties.Contains("homeDrive") Then
+            ent.InvokeSet("homeDrive", letter)
+        Else
+            ent.InvokeSet("homeDrive", letter)
+        End If
+        If ent.Properties.Contains("homeDirectory") Then
+            ent.InvokeSet("homeDirectory", "\\svr.capsuleco.com\CapsuleHome\" + ent.Properties("sAMAccountName").Value)
+        Else
+            ent.InvokeSet("homeDirectory", "\\svr.capsuleco.com\CapsuleHome\" + ent.Properties("sAMAccountName").Value)
+        End If
+
+    End Sub
+
+    ''' <summary>
+    ''' Get the list of DirectoryEntries matches Filter
+    ''' </summary>
+    ''' <param name="FilterString">AD Filter String(I set as all users)</param>
+    ''' <returns>List of DirectoryEntry</returns>
+    ''' <remarks>In this program, A DirectoryEntry represents an Account record</remarks>
     Private Function getDirectoryEntry(ByVal FilterString As String) As List(Of DirectoryEntry)
         Dim DSearcher As DirectorySearcher = New DirectorySearcher(DEntry)
-        TextBox1.Text += "ADS Load" + vbCrLf
+        TextBox1.Text += "ADS Load" + vbCrLf 'for debug
         DSearcher.Filter = FilterString
-        TextBox1.Text += "Filter Filted" + vbCrLf
+        TextBox1.Text += "Filter Filted" + vbCrLf 'for debug
         DSearcher.SearchScope = SearchScope.Subtree
         Dim Sresult() As SearchResult
         Dim SResultCollection As SearchResultCollection = DSearcher.FindAll()
         ReDim Sresult(SResultCollection.Count - 1)
-        TextBox1.Text += "search over" + vbCrLf
+        TextBox1.Text += "search over" + vbCrLf 'for debug
         SResultCollection.CopyTo(Sresult, 0)
         Dim RetDEList As New List(Of DirectoryEntry)
         TextBox1.Text += SResultCollection.Count.ToString + vbCrLf
         For i As Integer = 0 To UBound(Sresult)
             RetDEList.Add(New DirectoryEntry(Sresult(i).Path))
-            'TextBox1.Text += Sresult(i).Path + vbCrLf
         Next
         Return RetDEList
     End Function
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        getDirectoryEntry(TextBox2.Text)
+        BeginCall(TextBox2.Text)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        CSetHomeDrive(TextBox2.Text)
     End Sub
 End Class
